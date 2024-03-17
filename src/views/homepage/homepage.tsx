8cff421dd8446b265c0050e07a03211d52525a32
tomibudis <tomibudis@gmail.com>
2024-03-17 22:01:03 +0700

    chore: update unit test

--- src/views/homepage/homepage.tsx
+++ src/views/homepage/homepage.tsx
@@ -3,7 +3,7 @@ import React, { memo } from "react";
 const Homepage: React.FC = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Homepage</h1>
     </>
   );
 };
